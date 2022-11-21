3b266594f9ba1577bd56df70412baeeba3a3a650
Eszter Egyud <eszter.egyud@telus.com>
2022-11-21 16:07:50 -0700

    implement middleTest.js

--- middle.js
+++ middle.js
@@ -3,26 +3,23 @@
 //const assertArraysEqual = require('../assertArraysEqual');
 
 const middle = function(arr) {
-  
+
   let midArray = [];
   let i = 0;
 
   if (arr.length === 0 || arr.length === 1 || arr.length === 2) {
-    // console.log(midArray);
     return midArray;
   }
-  
+
   if (arr.length % 2 === 0) {
     i = arr.length / 2;
     midArray.push(arr[i - 1]);
     midArray.push(arr[i]);
-    // console.log(midArray);
     return midArray;
   }
-  
+
   i = (arr.length - 1) / 2;
   midArray.push(arr[i]);
-  // console.log(midArray);
   return midArray;
 };
 
