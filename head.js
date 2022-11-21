266c0c9a9a74621d54c74763727570a134326c45
Eszter Egyud <eszter.egyud@telus.com>
2022-11-21 14:50:06 -0700

    implemented tailTest

--- head.js
+++ head.js
@@ -1,17 +1,7 @@
 // Head function
 
-// const assertEqual = require('./assertEqual');
-
 const head = function(myArray) {
   return myArray[0];
 };
 
 module.exports = head;
-
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head([5, 6, 7]), 7);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
-// assertEqual(head([]), undefined);
-// assertEqual(head([5, 6, 7]), "tide");
-// assertEqual(head([100]), 100);
