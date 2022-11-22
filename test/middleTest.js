011f86fce457160fecdbba76ec1b04151044e1a8
Eszter Egyud <eszter.egyud@telus.com>
2022-11-21 18:20:28 -0700

    implemented middleTest.js and tailTest.js with Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,43 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]); // => should PASS
+describe("#middle", () => {
+  let result;
+
+  it("returns [2] for [1, 2, 3]", () => {
+    result = middle([1, 2, 3]);
+    assert.deepEqual(result, [2]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    result = middle([1, 2, 3, 4, 5, 6]);
+    assert.deepEqual(result, [3, 4]);
+  });
+
+    it("returns [4] for [1, 2, 3, 4, 5, 6]", () => {
+    result = middle([1, 2, 3, 4, 5, 6, 7]);
+    assert.deepEqual(result, [4]);
+  });
+
+  it("returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+    result = middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    assert.deepEqual(result, [5, 6]);
+  });
+
+});
+
+
+
+
+
+
+//origninal middleTest code preserved for comparison
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+
+
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => should PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => should PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); // => should PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]); // => should PASS
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,54 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail = require('../tail'); //tail removes the first element of an array
 
-// Test Case 1: Check the returned array elements
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  let result;
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
 
-// Test Case 2: numbers
-result = tail([1, 2, 3, 4, 5]);
-assertEqual(result.length, 4); // ensure we get back two elements
-assertEqual(result[0], 2); // ensure first element is "2"
-assertEqual(result[1], 3); // ensure second element is "3"
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    result = tail([1, 2, 3, 4, 5]);
+    assert.deepEqual(result, [2, 3, 4, 5]);
+  });
 
-// Test Case 3: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it('returns 3 for original array length', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
 
-// Test Case 4: one item
-result = tail([100]);
-assertEqual(result.length, 0); // ensure we get back two elements
+  it("returns empty array for [100]", () => {
+    result = tail([100]);
+    assert.deepEqual(tail([100]), []);
+  });
+
+});
+
+
+
+//original tailTest code preserved for comparison
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// // Test Case 1: Check the returned array elements
+// let result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 2: numbers
+// result = tail([1, 2, 3, 4, 5]);
+// assertEqual(result.length, 4); // ensure we get back 4 elements
+// assertEqual(result[0], 2); // ensure first element is "2"
+// assertEqual(result[1], 3); // ensure second element is "3"
+
+// // Test Case 3: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// // Test Case 4: one item
+// result = tail([100]);
+// assertEqual(result.length, 0);
